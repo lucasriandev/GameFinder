35a224a913c07b18eee1949c0e09db001810ddd5
Lucas <lucasrian.dev@gmail.com>
2026-02-24 17:11:15 -0300

    manipulando dom e ajuste de css

---
+++ script.js
@@ -0,0 +1,10 @@
+const input = document.querySelector("#input-jogo");
+const iniciarBusca = document.querySelector("#btn-buscar");
+
+const capaJogo = document.querySelector("#img-capa");
+const txtTitulo = document.querySelector("#txt-titulo");
+const dataLancamento = document.querySelector("#txt-data");
+const nota = document.querySelector("#txt-nota");
+
+const cardJogo = document.querySelector("#card-jogo");
+//cardJogo.style.display = 'flex'
