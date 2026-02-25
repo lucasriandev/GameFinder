b2ae1256becbd12a81cbd99444dbdd2cca6fad10
Lucas <lucasrian.dev@gmail.com>
2026-02-25 16:28:31 -0300

    projeto finalizado por hora

--- script.js
+++ script.js
@@ -1,7 +1,53 @@
 const input = document.querySelector("#input-jogo");
-const btnBusca = document.querySelector("#btn-buscat");
+const btnBusca = document.querySelector("#btn-buscar");
 
 const cardPrincipal = document.querySelector("#card-jogo");
 const imgCapa = document.querySelector("#img-capa");
 const tituloJogo = document.querySelector("#txt-titulo");
 const preco = document.querySelector("#txt-preco");
+
+let jogoEncontrado;
+
+async function api() {
+  const valor = input.value;
+  if (valor === "") {
+    alert("Digite o jogo que você deseja encontrar!");
+    return;
+  }
+  const url = `https://www.cheapshark.com/api/1.0/games?title=${valor}`;
+
+  try {
+    const resposta = await fetch(url);
+
+    if (!resposta.ok) {
+      throw new Error("ERROOOOO");
+    }
+
+    const dados = await resposta.json();
+    if (dados.length === 0) {
+      alert("Jogo não encontrado!");
+    }
+    console.log(dados);
+
+    jogoEncontrado = dados[0];
+    console.log(jogoEncontrado);
+
+    imgCapa.src = jogoEncontrado.thumb;
+    tituloJogo.innerText = jogoEncontrado.external;
+    preco.innerText = "$ " + jogoEncontrado.cheapest;
+
+    input.value = "";
+  } catch (error) {
+    console.log(error);
+  } finally {
+    cardPrincipal.classList.remove("escondido");
+  }
+}
+
+btnBusca.addEventListener("click", api);
+
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    btnBusca.click();
+  }
+});
